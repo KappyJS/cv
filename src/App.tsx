1e68a0e1386f7affacbd7a5e8bea74df7ca0c275
17633233 <Kapitanov.N.Dmi@sberbank.ru>
2022-03-07 16:52:56 +0300

    feat(common): Init repo

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello, this is my cv</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.(html)$/, use: ['html-loader'] },
+    ],
+  },
+};
