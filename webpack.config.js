748ab42cd0d9976ae6cca19edd7711030189f91d
17633233 <Kapitanov.N.Dmi@sberbank.ru>
2022-03-13 02:56:27 +0300

    feat(common): add font Geometria

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,13 @@ module.exports = {
         ],
       },
       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
